fdbefb547db72e3d9e5420da82e63eb17b3832a3
victorFeracin <victorhugo.feracin@gmail.com>
2026-04-03 19:00:43 -0300

    refactor(login): Implementing baseURL for cy.visit on Login tests

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
 
   e2e: {
     //video: true, //Enable video recording
+    baseUrl: 'http://localhost:4000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
